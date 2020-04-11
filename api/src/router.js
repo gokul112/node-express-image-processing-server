6a21de7f05bb15d3554be5e7c1ab3a83b3af9ff8
gokul raj <gokul.raj@gigamon.com>
2020-04-11 19:49:24 +0530

    Module-4 completed

--- api/src/router.js
+++ api/src/router.js
@@ -1,34 +1,49 @@
 const {Router} = require('express');
 const multer = require('multer');
 const path = require('path');
+const imageProcessor = require('./imageProcessor');
+
+const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
+
 const router = Router();
-const storage = multer.diskStorage({destination:'api/uploads/',filename:filename});
-const upload = multer({fileFilter,storage});
 
-const photoPath = path.resolve(__dirname,'../../client/photo-viewer.html');
+const filename = (request, file, callback) => {
+  callback(null, file.originalname);
+};
 
-router.get('/photo-viewer',(request,response)=>{
-    response.sendFile(photoPath);
-})
-
-function filename(request,file,callback){
-    callback(null,file.originalname);
-}
-
-function fileFilter(request,file,callback){
-    if(file.mimetype!=="image/png"){
-        request.fileValidationError = "Wrong file type";
-        callback(null,false, new Error('Wrong file type'));
-        Error("Wrong file type");
-    }
-    else{
-        callback(null,true);
-    }
-}
-
-router.post('/upload',upload.single('photo'),(request, response) => {
-    if (request.fileValidationError) return response.status(400).json({error: request.fileValidationError});
-    
-    return response.status(201).json({success: true});
+const storage = multer.diskStorage({
+  destination: 'api/uploads/',
+  filename,
 });
+
+const fileFilter = (request, file, callback) => {
+  if (file.mimetype !== 'image/png') {
+    request.fileValidationError = 'Wrong file type';
+    callback(null, false, new Error('Wrong file type'));
+  } else {
+    callback(null, true);
+  }
+};
+
+const upload = multer({
+  fileFilter,
+  storage,
+});
+
+router.post('/upload', upload.single('photo'), async (request, response) => {
+  if (request.fileValidationError) return response.status(400).json({error: request.fileValidationError});
+
+  try {
+    await imageProcessor(request.file.filename);
+  } catch (error) {
+
+  }
+
+  return response.status(201).json({success: true});
+});
+
+router.get('/photo-viewer', (request, response) => {
+  response.sendFile(photoPath);
+});
+
 module.exports = router;
